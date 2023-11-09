1a2d5c51164a211e49487851f339f541b5b78a45
Meenakshi2215 <meenakshika532@gmail.com>
2023-11-09 12:46:41 +0530

    final backend version

---
+++ index.js
@@ -0,0 +1,32 @@
+//import json-server library in index.js
+
+const jsonServer = require('json-server')
+
+//create server using json-server library
+
+const mediaPlayerServer = jsonServer.create()
+
+//create path to db.json file using .router() as an argument
+
+const router = jsonServer.router('db.json')
+
+//middlewares to convert js to json
+const middleware = jsonServer.defaults()
+
+//connect or use middleware and router in server
+
+mediaPlayerServer.use(middleware)
+mediaPlayerServer.use(router)
+
+// setup port for the server (bydefault aayitt react and json is run in 3000 port)
+
+const port = 5000 || process.env.PORT
+
+//to listen server for resolving request
+
+mediaPlayerServer.listen(port,()=>{
+    console.log(`mediaPlayerServer started at ${port} and ready to fetch request`);
+})
+
+
+
